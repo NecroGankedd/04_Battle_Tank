57278b2ecbe26be48596635b0ad000f5159c6e0f
NecroGankedd <necrogankedd@gmail.com>
2017-09-17 01:17:14 -0700

    Added AIController, AI Tanks, GetFirstPlayerController()

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI NOT finding PLAYER tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller found PlayerTank: %s"), (*PlayerTank->GetName()));
+	}
+
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
+
---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player NOT currently controlling a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player currently controlling %s"), *GetControlledTank()->GetName());
+	}
+	
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
